9985c2a8a4d92cf029b5a38bf39cc275ada1f88b
ary418 <aryachan.desu@gmail.com>
2025-04-19 12:19:03 +0000

    flake: fix binary cache

--- flake.nix
+++ flake.nix
@@ -20,10 +20,10 @@
   };
 
   nixConfig = {
-    substituters = [
+    extra-substituters = [
       "https://nix-community.cachix.org"
     ];
-    trusted-public-keys = [
+    extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
